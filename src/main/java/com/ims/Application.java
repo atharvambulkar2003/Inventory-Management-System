41cb3844b69c37bb7c25c8d8f952c65c56f7bf3b
Atharva <ambulkaratharv2003@gmail.com>
2026-03-28 18:06:41 +0530

    LocaleDateTime issue solve

--- src/main/java/com/ims/Application.java
+++ src/main/java/com/ims/Application.java
@@ -1,12 +1,21 @@
 package com.ims;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableAsync
 public class Application {
+	
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
